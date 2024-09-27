03272a51946b8e55d055d0a95a2b34698cbbc2dc
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-09-27 20:26:49 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+ 
+Console.Write("Texto a ser traduzido: ");
+ 
+Console.WriteLine(
+Console.ReadLine()!
+.Replace("a", "4")
+.Replace("e", "3")
+.Replace("i", "1")
+.Replace("o", "0")
+.Replace("t", "7")
+.Replace("s", "5")
+ 
+);
